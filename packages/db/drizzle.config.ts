0df11f1a0a2a24cb3dcffbdfd8ac3e52fe15f126
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-04-17 20:06:23 +0530

    chore: pointing db url to root

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv';
 import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env')});
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export default defineConfig({
   out: './drizzle',
