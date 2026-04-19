5504d85191a5a757b73461a3caa376ce7658a9ae
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-04-19 22:48:07 +0530

    feat: add queue constants

---
+++ packages/queue/src/constants.ts
@@ -0,0 +1,5 @@
+export const FANOUT_QUEUE_NAME = "fanout-queue";
+export const FANOUT_WORKER_NAME = "fanout-worker";
+export const DELIVERY_QUEUE_NAME = "delivery-queue";
+export const DELIVERY_WORKER_NAME = "delivery-worker";
+export const DLQ_NAME = "dlq";
--- packages/queue/src/index.ts
+++ packages/queue/src/index.ts
@@ -1,5 +1,10 @@
 export { bullmqRedis } from "./client";
 export { fanoutQueue } from "./fanout.queue";
 export type { FanoutJobData } from "./fanout.queue";
-export { getDeliveryQueueName, getDeliveryQueue, closeAllDeliveryQueues } from "./delivery.queue";
+export {
+  getDeliveryQueueName,
+  getDeliveryQueue,
+  closeAllDeliveryQueues,
+} from "./delivery.queue";
 export type { DeliveryJobData } from "./delivery.queue";
+export * from "./constants";
