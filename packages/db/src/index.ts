ba99b7954107c873f6f0f531c307681a675ea746
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-04-24 19:52:02 +0530

    fix: export types and others from schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,9 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle({ client: sql });
 
 export { db };
-export * from './queries';
+export * from "./queries";
+export * from "./schema";
