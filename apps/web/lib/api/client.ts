6a0aa03be839413eb9274f79d0105854ef83f6a2
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-05-09 23:27:42 +0530

    feat: api client

---
+++ apps/web/lib/api/client.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const apiClient = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    // If we receive a 401, the user's session might be expired.
+    // The middleware will handle redirects on navigation, but we can also
+    // trigger a redirect here if needed. For now, we just pass the error.
+    if (error.response?.status === 401 && typeof window !== "undefined") {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
